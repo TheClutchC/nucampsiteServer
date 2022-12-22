3456de3e190ce1a099abcd7a0ca741bc4f4ec02b
Clay Carney <claycarney7@gmail.com>
2022-12-22 10:18:55 -0500

    Express Sessions Part 1

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users.js');
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 
 const mongoose = require("mongoose");
 
@@ -33,10 +35,20 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("12345-67890-54321-09876"));
+// app.use(cookieParser("12345-67890-54321-09876"));
+
+app.use(session({
+    name: "session-id",
+    secret: "12345-67890-54321-09876",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+}));
 
 function auth(req, res, next) {
-    if (!req.signedCookies.user) {
+    console.log(req.session);
+
+    if (!req.session.user) {
         const authHeader = req.headers.authorization;
         if (!authHeader) {
             const err = new Error("You are not authenticated!");
@@ -49,8 +61,8 @@ function auth(req, res, next) {
         const user = auth[0];
         const pass = auth[1];
         if (user === "admin" && pass === "password") {
-            res.cookie('user', 'admin', { signed: true });
-            return next();
+            req.session.user = "admin";
+            return next(); // authorized
         } else {
             const err = new Error("You are not authenticated!");
             res.setHeader("WWW-Authenticate", "Basic");
@@ -58,7 +70,7 @@ function auth(req, res, next) {
             return next(err);
         }
     } else {
-        if (req.signedCookies.user === 'admin') {
+        if (req.session.user === 'admin') {
             return next();
         } else {
             const err = new Error('You are not authenticated!');
